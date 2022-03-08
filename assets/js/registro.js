9458e7d85c5f90b8db1f5dc12af605b1955b9600
AntonioVillegas13 <88470677+AntonioVillegas13@users.noreply.github.com>
2022-03-08 08:41:03 -0500

    Update registro.js

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,30 +1,51 @@
-function ir2(){
+const mapa= new Map();
+
+mapa.set('Antonio','12345');
+function ir3(){
+   
     let usuario = document.getElementById("nombre").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
-    console.log(usuario)
-    if(usuario== "GRUPO4" && email=="grupo4@hotmail.com" && password=="grupo4"){
-        alert("Datos Correctos");
-        window.location="Aplicaciones.html";
+    alert("Datos Registrados");
+    mapa.set(usuario,password);
+    mapa.forEach((valor,clave)=> {
+        console.log(`la clave es :${clave} y el valor asociado es ${valor}`);
+    })
     }
-        else{
+
+
+function ir2(){
+   
+    let usuario = document.getElementById("nombre").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    console.log(usuario);
+    mapa.forEach((valor,clave)=> {
+        console.log(valor);
+        
+        if(usuario==clave  && password==valor){
+            alert("Datos Correctos");
+            window.location="Aplicaciones.html";
+            
+        } else{
             alert("Porfavor ingrese los  nombre de usuario y contraseña correctos."); 
 
         }
+    })
+
+
+
+   
 
     }
 
-    function ir3(){
-        let usuario = document.getElementById("nombre").value;
-        let email = document.getElementById("email").value;
-        let password = document.getElementById("password").value;
-        alert("Datos Registrados")
-
-    
-        }
-    
+   
         
     
+
+
+
+
 
 
 
